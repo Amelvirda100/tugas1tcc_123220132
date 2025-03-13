371cb654b518ce69bf275cb1bd57bb404c99e4c0
Amelvirda100 <163957872+Amelvirda100@users.noreply.github.com>
2025-03-13 07:49:16 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("tugas_notes", "root", "", {
-  host: "localhost",
+const db = new Sequelize("tugas_notes", "root", "123220132", {
+  host: "34.172.234.183",
   dialect: "mysql",
 });
 
