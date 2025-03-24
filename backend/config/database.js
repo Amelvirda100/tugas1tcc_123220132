11691c0a6e0dbd2e2f7a85a476bc4cc45dfdafd4
Amelvirda100 <163957872+Amelvirda100@users.noreply.github.com>
2025-03-25 02:37:38 +0700

    Update database.js
    ganti ip gara2 instance lama kehapus

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize("tugas_notes", "root", "123220132", {
-  host: "34.172.234.183",
+  host: "34.41.167.130",
   dialect: "mysql",
 });
 
